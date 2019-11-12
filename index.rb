d1f7f5a3655fdaa3a60b0f1120e38c0eed51f4b4
morikentiger <kenrur339@gmail.com>
2019-11-12 23:37:22 +0900

    def initialize

--- index.rb
+++ index.rb
@@ -1,15 +1,18 @@
 class Person
   attr_accessor :name
   attr_accessor :age
+  def initialize(name)
+    self.name = name
+  end
   def introduce
     puts "こんにちは"
     puts "私は#{name}です"
   end
 end
-person1 = Person.new
-person1.name = "にんじゃわんこ"
+person1 = Person.new("にんじゃわんこ")
 person1.introduce
 
-# introduce("ひつじ仙人")
+person1 = Person.new("ひつじ仙人")
+person1.introduce
 # introduce("ベイビーわんこ")
 # introduce("morikentiger")
