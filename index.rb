cae35e2afa92bbbfbe5b13c671d0365197df17d5
morikentiger <kenrur339@gmail.com>
2019-11-13 10:38:19 +0900

    modify class file person.rb

--- index.rb
+++ index.rb
@@ -1,9 +1,9 @@
 require "./person"
 
-person1 = Person.new("にんじゃわんこ")
+person1 = Person.new("にんじゃわんこ", 14)
 person1.introduce
 
-person1 = Person.new("ひつじ仙人")
+person1 = Person.new("ひつじ仙人", 28)
 person1.introduce
 # introduce("ベイビーわんこ")
 # introduce("morikentiger")
--- person.rb
+++ person.rb
@@ -1,11 +1,12 @@
 class Person
   attr_accessor :name
   attr_accessor :age
-  def initialize(name)
+  def initialize(name, age)
     self.name = name
+    self.age = age
   end
   def introduce
     puts "こんにちは"
-    puts "私は#{name}です"
+    puts "私は#{name}で#{age}歳です"
   end
 end
