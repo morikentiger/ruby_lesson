da83f15a4a06e88eae84b0e0f030058dbd6f0e68
morikentiger <kenrur339@gmail.com>
2019-11-13 10:44:57 +0900

    organaize index.rb

--- index.rb
+++ index.rb
@@ -1,9 +1,11 @@
 require "./person"
 
 person1 = Person.new("にんじゃわんこ", 14)
-person1.introduce
+person2 = Person.new("ひつじ仙人", 65)
+person3 = Person.new("ベイビーわんこ", 9)
+person4 = Person.new("morikentiger", 28)
 
-person1 = Person.new("ひつじ仙人", 28)
 person1.introduce
-# introduce("ベイビーわんこ")
-# introduce("morikentiger")
+person2.introduce
+person3.introduce
+person4.introduce
